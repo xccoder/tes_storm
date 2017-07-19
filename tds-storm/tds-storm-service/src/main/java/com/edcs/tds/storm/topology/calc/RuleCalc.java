b1cdbaf2a52798aecd28b039d60a9083815f64c3
CA <CaiSL2@CIT-I151480-N.CATLBattery.com>
2017-07-19 15:28:40 +0800

    no message

--- tds-storm/tds-storm-service/src/main/java/com/edcs/tds/storm/topology/calc/RuleCalc.java
+++ tds-storm/tds-storm-service/src/main/java/com/edcs/tds/storm/topology/calc/RuleCalc.java
@@ -43,38 +43,38 @@ public class RuleCalc {
         TestingMessage testingMessage = executeContext.getTestingMessage();//实时数据
         Set<String> processInfoJsons = cacheService.getProcessInfoJsons();//获取流程主数据 信息
         MDprocessInfo mDprocessInfo = null;
-		if(processInfoJsons!=null && processInfoJsons.size()>0){
-			for (String string : processInfoJsons) {
-				MDprocessInfo mDprocessInfo2 = JsonUtils.toObject(string, MDprocessInfo.class);
-				if (testingMessage.getRemark().equals(mDprocessInfo2.getRemark())) {
-	                //获取当前测试的流程
-	                mDprocessInfo = mDprocessInfo2;
-	                break;
-	            }
-			}
-		}else{
-			return;
-		}
+        if (processInfoJsons != null && processInfoJsons.size() > 0) {
+            for (String string : processInfoJsons) {
+                MDprocessInfo mDprocessInfo2 = JsonUtils.toObject(string, MDprocessInfo.class);
+                if (testingMessage.getRemark().equals(mDprocessInfo2.getRemark())) {
+                    //获取当前测试的流程
+                    mDprocessInfo = mDprocessInfo2;
+                    break;
+                }
+            }
+        } else {
+            return;
+        }
         Jedis jedis = cacheService.getProxyJedisPool().getResource();
         List<TestingResultData> listResult = new ArrayList<TestingResultData>();//用来存放结果数据
         //int matchedCount = 0;
         // key = 流程号+序号
-        String key = testingMessage.getRemark() +"_"+testingMessage.getSequenceId();
+        String key = testingMessage.getRemark() + "_" + testingMessage.getSequenceId();
         int sequenceNumber = 0;//同一个工步中报警的序号
         //遍历每一个场景
         Set<String> keys = ruleConfig.keySet();//获取所有的key 的值
         for (String string : keys) {
-			List<RuleConfig> rule = ruleConfig.get(string);//获取每一个场景的值
-			String str = string.substring(string.lastIndexOf("_")+1);
-			String sceneName = str;//场景名称
-			for (RuleConfig ruleConfig2 : rule) {//遍历一个场景下的所有规则
-				if(testingMessage.getStepName().equals(ruleConfig2.getStepName())){
-					long executeUsedTime = 0;
-	                long executeBeginTime = System.currentTimeMillis();
-	                String alterLevel = null;
+            List<RuleConfig> rule = ruleConfig.get(string);//获取每一个场景的值
+            String str = string.substring(string.lastIndexOf("_") + 1);
+            String sceneName = str;//场景名称
+            for (RuleConfig ruleConfig2 : rule) {//遍历一个场景下的所有规则
+                if (testingMessage.getStepName().equals(ruleConfig2.getStepName())) {
+                    long executeUsedTime = 0;
+                    long executeBeginTime = System.currentTimeMillis();
+                    String alterLevel = null;
                     try {
-                    	Script script = CacheService.getScriptCache().get(ruleConfig2.getStepSign()).getRight();
-                    	script.setBinding(shellContext);
+                        Script script = CacheService.getScriptCache().get(ruleConfig2.getStepSign()).getRight();
+                        script.setBinding(shellContext);
                         // 返回值为 ： 报警上限_报警下限_比较值_报警级别
                         alterLevel = scriptExecutor.execute(script);
                     } catch (Exception e) {
@@ -85,53 +85,53 @@ public class RuleCalc {
                         executeUsedTime = System.currentTimeMillis() - executeBeginTime;
                     }
                     if (StringUtils.isNotBlank(alterLevel) && !alterLevel.equals("null")) {
-                    	sequenceNumber++;
-                    	TestingResultData testingResultData = new TestingResultData();
+                        sequenceNumber++;
+                        TestingResultData testingResultData = new TestingResultData();
                         //matchedCount++;
                         // TODO 记录匹配的规则和相关数据
 //                        executeContext.addMatchedRule(ruleConfig2.getId(), 0d, executeUsedTime);
                         // TODO 将结果集写入Redis缓存
                         String[] alters = alterLevel.split("_");
                         BigDecimal upLimit = new BigDecimal(alters[0]);    //.valueOf(Long.valueOf(alters[0]));//报警上限
-                        BigDecimal lowLimit = new  BigDecimal(alters[1]); //.valueOf(Long.valueOf(alters[1]));//报警下限
+                        BigDecimal lowLimit = new BigDecimal(alters[1]); //.valueOf(Long.valueOf(alters[1]));//报警下限
                         int alterLe = Integer.parseInt(alters[3]);//报警级别
                         //调用发送邮件接口发送预警信息  -- start
                         //通过redis获取收件人信息
-                        Set<String> sets = jedis.smembers("warningLevel_"+alterLe);
-                        
+                        Set<String> sets = jedis.smembers("warningLevel_" + alterLe);
+
 //                        Set<String> sets = new HashSet<String>();
 //                        sets.add("XiangC@CATLBattery.com");
 //                        sets.add("CaiSL2@CATLBattery.com");
 //                        sets.add("LiQF@CATLBattery.com");
-                        String content = "通道号为："+testingMessage.getChannelId()+";</br>流程号为："+testingMessage.getRemark()+";</br>公布名称为："+ruleConfig2.getStepName()+";</br>场景名称为："+sceneName+";</br>产生了"+alterLe+"级预警！！";//预警信息。
-                        System.out.println("报警信息为："+content+"-----------------------------------------------------------");
-                        if(sets!=null && sets.size()>0){
-                        	List<String> receiveAccounts = new ArrayList<String>();//存放收件人帐号
-                        	EmailEntity emailEntity = new EmailEntity();
-                        	for (String string2 : sets) {
-                        		UserIntegrationRedis userMsg = JsonUtils.toObject(string2, UserIntegrationRedis.class);
-                        		receiveAccounts.add(userMsg.getEmail());
-                        	}
-                        	receiveAccounts.addAll(sets);
-                        	emailEntity.setReceiveAccounts(receiveAccounts);
-                        	emailEntity.setBooeanSsl(false);
-                        	emailEntity.setContent(content);
-                        	
-                        	emailEntity.setSendServer(myEmailSMTPHost);
-                        	emailEntity.setSendAccount(myEmailAccount);
-                        	emailEntity.setEmailPassword(myEmailPassword);
-                        	//发送邮件
-                        	try {
-								SendEmailUtils.sendEmail(emailEntity);
-							} catch (Exception e) {
-								e.printStackTrace();
-							}
+                        String content = "通道号为：" + testingMessage.getChannelId() + ";</br>流程号为：" + testingMessage.getRemark() + ";</br>公布名称为：" + ruleConfig2.getStepName() + ";</br>场景名称为：" + sceneName + ";</br>产生了" + alterLe + "级预警！！";//预警信息。
+                        System.out.println("报警信息为：" + content + "-----------------------------------------------------------");
+                        if (sets != null && sets.size() > 0) {
+                            List<String> receiveAccounts = new ArrayList<String>();//存放收件人帐号
+                            EmailEntity emailEntity = new EmailEntity();
+                            for (String string2 : sets) {
+                                UserIntegrationRedis userMsg = JsonUtils.toObject(string2, UserIntegrationRedis.class);
+                                receiveAccounts.add(userMsg.getEmail());
+                            }
+                            receiveAccounts.addAll(sets);
+                            emailEntity.setReceiveAccounts(receiveAccounts);
+                            emailEntity.setBooeanSsl(false);
+                            emailEntity.setContent(content);
+
+                            emailEntity.setSendServer(myEmailSMTPHost);
+                            emailEntity.setSendAccount(myEmailAccount);
+                            emailEntity.setEmailPassword(myEmailPassword);
+                            //发送邮件
+                            try {
+                                SendEmailUtils.sendEmail(emailEntity);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                         }
                         //调用发送邮件接口发送预警信息  -- end
 
-                        
+
                         String handle = "TxAlertInfoBO:" + mDprocessInfo.getSite() + "," + mDprocessInfo.getRemark() + "," + testingMessage.getSfc() + "," + sceneName + "," + sequenceNumber;
-                        
+
                         testingResultData.setHandle(handle);
                         testingResultData.setSite(mDprocessInfo.getSite());
                         testingResultData.setCategory(sceneName);
@@ -162,19 +162,21 @@ public class RuleCalc {
                 }
             }
         }
-        if(listResult.size()==0 && mDprocessInfo!=null){//表明这条测试数据一个预警信息都没有，那么我们要产生一个result对象来存放原始测试数据信息
-        	TestingResultData testingResultData = new TestingResultData();
-        	testingResultData.setSite(mDprocessInfo.getSite());
-        	testingResultData.setRootRemark(mDprocessInfo.getRootRemark());
-        	testingResultData.setProcessDataBO("MdProcessInfoBO:" + mDprocessInfo.getSite() + "," + mDprocessInfo.getProcessId() + "," + mDprocessInfo.getRemark());
-        	testingResultData.setErpResourceBO("ErpResourceBO:" + mDprocessInfo.getSite() + "," + testingMessage.getResourceId());
-        	testingResultData.setOriginalProcessDataBO("TxOriginalProcessDataBO:" + mDprocessInfo.getSite() + "," + mDprocessInfo.getRemark() + "," + mDprocessInfo.getSfc() + "," + testingMessage.getResourceId() + "," + testingMessage.getChannelId() + "," + testingMessage.getSequenceId());
-        	testingResultData.setTestingMessage(testingMessage);
-        	listResult.add(testingResultData);
+        if (listResult.size() == 0 && mDprocessInfo != null) {//表明这条测试数据一个预警信息都没有，那么我们要产生一个result对象来存放原始测试数据信息
+            TestingResultData testingResultData = new TestingResultData();
+            testingResultData.setSite(mDprocessInfo.getSite());
+            testingResultData.setRootRemark(mDprocessInfo.getRootRemark());
+            testingResultData.setProcessDataBO("MdProcessInfoBO:" + mDprocessInfo.getSite() + "," + mDprocessInfo.getProcessId() + "," + mDprocessInfo.getRemark());
+            testingResultData.setErpResourceBO("ErpResourceBO:" + mDprocessInfo.getSite() + "," + testingMessage.getResourceId());
+            testingResultData.setOriginalProcessDataBO("TxOriginalProcessDataBO:" + mDprocessInfo.getSite() + "," + mDprocessInfo.getRemark() + "," + mDprocessInfo.getSfc() + "," + testingMessage.getResourceId() + "," + testingMessage.getChannelId() + "," + testingMessage.getSequenceId());
+            testingResultData.setTestingMessage(testingMessage);
+            listResult.add(testingResultData);
         }
-        String result = JsonUtils.toJson(listResult);
-        jedis.set(key, result);//用于计算过程中查询历史数据
-        jedis.expire(key, 60 * 60);
-        jedis.sadd("TES-RESULT", result);//用于同步服务写入hana
+        if (listResult.size() > 0) {
+            String result = JsonUtils.toJson(listResult);
+            jedis.set(key, result);//用于计算过程中查询历史数据
+            jedis.expire(key, 60 * 60);
+            jedis.sadd("TES-RESULT", result);//用于同步服务写入hana
+        }
     }
 }
